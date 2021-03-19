53f38903135a40a6f44e6b00345ec1d6dedc5f45
Allekksa <allekksa@gmail.com>
2021-03-19 19:49:58 +0300

    Разобранны будиева логика, тернарные вырвжения, условые операторы, приоритет операторов

---
+++ app.js
@@ -0,0 +1,12 @@
+// let a = 25
+// a = 74
+// // let name = "Aleksa"
+// console.log(a)
+// // console.log(name)
+const isReady = true
+// if (isReady) {
+//   console.log("Готово!")
+// } else {
+//  console.log("Не готово!")
+//     }
+isReady ? console.log("Готово!") : console.log("Не готово!")
