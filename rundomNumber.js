0beeed15fb5f7d57ac45a357ab9c37671c481454
Allekksa <allekksa@gmail.com>
2021-04-26 17:33:44 +0300

    задачи на циклы и мат методы

--- rundomNumber.js
+++ rundomNumber.js
@@ -1,4 +1,12 @@
-function getRandomBetween(max, min){
+//случ целое
+
+function getRandomBetween(min, max){
     return Math.floor(Math.random() * (max - min +1) + min)
 }
-console.log(getRandomBetween(42,1))
+console.log(getRandomBetween(1, 42))
+
+// случ дробное
+function getRandomArbitary(min, max){
+    return Math.random(Math.random() * (max - min) + min)
+}
+console.log(getRandomArbitary(1, 42))
